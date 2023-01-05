41ced00fd3c9afd23b50bdee9a42055f16bc25f6
mizbauddintareqe <mizbauddintareq@gmail.com>
2023-01-05 13:54:49 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div>
+        <button className="btn mx-4 btn-xs=">Button</button>
+        <button className="btn mx-4 btn-xs= btn-primary">Button</button>
+        <button className="btn mx-4 btn-xs= btn-secondary">Button</button>
+        <button className="btn mx-4 btn-xs= btn-accent">Button</button>
+        <button className="btn mx-4 btn-xs= btn-ghost">Button</button>
+        <button className="btn mx-4 btn-xs= btn-link">Button</button>
+      </div>
     </div>
   );
 }
